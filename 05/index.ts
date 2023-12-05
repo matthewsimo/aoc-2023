95fd78fe01f388040e0d982cc423dd1d73f155e9
Matthew Simo <matthew.a.simo@gmail.com>
2023-12-05 12:40:09 -0600

    05

---
+++ 05/index.ts
@@ -0,0 +1,89 @@
+const extractNumbers = (s: string) => s.split(" ").map((n) => Number(n));
+
+const findMapping = (x: number, mappings: number[][]) => {
+  // Is x in n2 - n2+o?
+  //    - return n1 + (x - n2)
+  // else
+  //    - return x
+  let mappedNum = x;
+
+  for (const mapping of mappings) {
+    const [n1, n2, o] = mapping;
+    if (x >= n2 && x < n2 + o) {
+      mappedNum = n1 + (x - n2);
+      break;
+    }
+  }
+
+  return mappedNum;
+};
+
+export const p1 = (input: string) => {
+  let [s, ...m] = input.split("\n\n");
+  const seeds = extractNumbers(s.split("seeds: ")[1]);
+
+  const maps = m.map((map) => {
+    const [_, ...mapNums] = map.split("\n");
+    return mapNums.map((line) => extractNumbers(line));
+  });
+
+  const locations = seeds.map((seed) => {
+    let x = seed;
+    let mappings = maps.map((m, i) => {
+      x = findMapping(x, m);
+      return x;
+    });
+    return mappings.at(-1) as number;
+  });
+
+  // console.log({ seeds, locations, maps });
+  return Math.min(...locations);
+};
+
+export const p2 = (input: string) => {
+  let [s, ...m] = input.split("\n\n");
+  let rawSeeds = extractNumbers(s.split("seeds: ")[1]);
+
+  let seeds: number[][] = [];
+  for (let i = 0; i < rawSeeds.length; i = i + 2) {
+    seeds.push([rawSeeds[i], rawSeeds[i] + rawSeeds[i + 1]]);
+  }
+
+  const maps = m.map((map) => {
+    const [_, ...mapNums] = map.split("\n");
+    return mapNums.map((line) => extractNumbers(line));
+  });
+
+  // console.log({ seeds, maps });
+
+  for (let mapGroup of maps) {
+    let nextSet: number[][] = [];
+    while (seeds.length > 0) {
+      let [seedStart, seedEnd] = seeds.pop() as number[];
+
+      let found = false;
+      inner: for (let [n1, n2, o] of mapGroup) {
+        const mS = Math.max(seedStart, n2);
+        const mE = Math.min(seedEnd, n2 + o);
+        // console.log({ mS, mE, n1, n2, o, seedStart, seedEnd });
+        if (mS < mE) {
+          nextSet.push([mS - n2 + n1, mE - n2 + n1]);
+          if (mS > seedStart) {
+            seeds.push([seedStart, mS]);
+          }
+          if (seedEnd > mE) {
+            seeds.push([mE, seedEnd]);
+          }
+          found = true;
+          break inner;
+        }
+      }
+      if (!found) {
+        nextSet.push([seedStart, seedEnd]);
+      }
+    }
+    seeds = nextSet;
+  }
+
+  return Math.min(...seeds.map((s) => s[0]));
+};
