0f53f30501f95307f75c3f1ed090c3bd698f3888
Matthew Simo <matthew.a.simo@gmail.com>
2023-12-05 16:30:17 -0600

    Add nicer day reporting

--- 05/main.ts
+++ 05/main.ts
@@ -1,8 +1,8 @@
 import { p1, p2 } from "./index";
 import input from "../_input/05.txt";
-import { time } from "../utils/perf";
+import { time, report } from "../utils/perf";
 
-const [p1A, p1T] = time(() => p1(input));
-console.log(`p1: ${p1A} -- ${p1T}ms`);
-const [p2A, p2T] = time(() => p2(input));
-console.log(`p2: ${p2A} -- ${p2T}ms`);
+report(
+  () => p1(input),
+  () => p2(input)
+);
--- utils/perf.ts
+++ utils/perf.ts
@@ -1,6 +1,28 @@
+import chalk from "chalk";
+import { printTable } from "console-table-printer";
+
 export const time = (fn: () => any) => {
   const s = performance.now();
   const a = fn();
   const e = performance.now();
   return [a, e - s];
+};
+
+export const report = (fn1: () => any, fn2: () => any) => {
+  const [a1, dur1] = time(() => fn1());
+  const [a2, dur2] = time(() => fn2());
+  console.log();
+  printTable([
+    {
+      run: chalk.bold.bgGreen(" p1 "),
+      answer: a1,
+      duration: chalk.dim(`[${dur1}ms]`),
+    },
+
+    {
+      run: chalk.bold.bgGreen(" p2 "),
+      answer: a2,
+      duration: chalk.dim(`[${dur2}ms]`),
+    },
+  ]);
 };
