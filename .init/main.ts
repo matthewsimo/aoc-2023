321af089eb11c3b9ebb3b51e3bdc56776a5fab37
Matthew Simo <matthew.a.simo@gmail.com>
2023-12-06 09:29:23 -0600

    Update init main template to use report util

--- .init/main.ts
+++ .init/main.ts
@@ -1,5 +1,8 @@
 import { p1, p2 } from "./index";
 import input from "../_input/{day}.txt";
+import { report } from "../utils/perf";
 
-console.log(`p1: ${p1(input)}`);
-console.log(`p2: ${p2(input)}`);
+report(
+  () => p1(input),
+  () => p2(input)
+);
